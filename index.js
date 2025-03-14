7e1936471f121f9e3023113358320ea6ea70e24e
memoryDube <127111522+memoryDube@users.noreply.github.com>
2025-03-14 15:38:23 +0200

    edited html,css and js to fine tune ir

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function handlesearch(event){
-    event.preventDefault()
-    let searchInput = document.querySelector("searchinput")
-    let searchCity = document.querySelector("current-city")
-    searchCity.innerHTML = searchInput.value
+function handlesearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let searchCity = document.querySelector("#current-city");
+  searchCity.innerHTML = searchInput.value;
 }
 
-let searchform = document.querySelector("#search-form")
-searchform.addEventListener("submit", handlesearch)
+let searchform = document.querySelector("#search-form");
+searchform.addEventListener("submit", handlesearch);
