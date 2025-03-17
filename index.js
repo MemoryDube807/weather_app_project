042d7a84eeecdecacd1acfeced8cc7b53edda00c
memoryDube <127111522+memoryDube@users.noreply.github.com>
2025-03-17 12:13:12 +0200

    added api call for weather

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-function handlesearch(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let searchCity = document.querySelector("#current-city");
-  searchCity.innerHTML = searchInput.value;
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#current-city");
+
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
 }
 
-let searchform = document.querySelector("#search-form");
-searchform.addEventListener("submit", handlesearch);
+function searchCity(city) {
+  let apiKey = "609f9oe8a43ce187feb36f8ctad4a8a0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+if (searchForm) {
+  searchForm.addEventListener("submit", handleSearch);
+}
+
+searchCity(Bulawayo);
