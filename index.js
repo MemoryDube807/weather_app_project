8f886c6be8944ac9f234727e190aa85f0363ab3f
memoryDube <127111522+memoryDube@users.noreply.github.com>
2025-03-17 15:36:58 +0200

    injected more weather data

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#current-city");
+  let rainElement = document.querySelector("#rain");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
 
   let temperature = response.data.temperature.current;
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  rainElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+
+  console.log(response.data);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`; 
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`; 
 }
 
 function searchCity(city) {
   let apiKey = "609f9oe8a43ce187feb36f8ctad4a8a0";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; 
+
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -25,4 +59,5 @@ if (searchForm) {
   searchForm.addEventListener("submit", handleSearch);
 }
 
-searchCity(Bulawayo);
+// Default city
+searchCity("Bulawayo");
