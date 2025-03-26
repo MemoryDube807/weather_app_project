2315da2f4c21b9833a0399f8d5c619198517e0a8
memoryDube <127111522+memoryDube@users.noreply.github.com>
2025-03-26 10:15:01 +0200

    added html forecast on javascript

--- index.js
+++ index.js
@@ -61,5 +61,27 @@ if (searchForm) {
   searchForm.addEventListener("submit", handleSearch);
 }
 
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml += `
+      <div class=" weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature"> <strong>24°C</strong></div>
+              <div class="weather-forecast-temperature">9°C</div>
+            </div>
+          </div>      
+  `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
+
 // Default city
 searchCity("Nkayi");
